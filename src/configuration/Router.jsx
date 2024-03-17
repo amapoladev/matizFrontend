b982e804165c7b088374cf17c0fd76dd90244c2f
sandovaljohana <sandovaljohana0207@gmail.com>
2024-03-17 23:54:25 +0100

    update with branches cards and navbar

--- src/configuration/Router.jsx
+++ src/configuration/Router.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../components/views/home/Home';
+import Form from '../components/views/form/Form'
 
 const Router = () => {
   return (
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Home/>}></Route>
+    <Route path='/form' element={<Form/>}></Route>
     </Routes>
    </BrowserRouter>
   )
