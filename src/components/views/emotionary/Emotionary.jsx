629f802a8aace7dc221bb919b827389f8dc832ac
sandovaljohana <sandovaljohana0207@gmail.com>
2024-03-18 00:04:46 +0100

    add view emotionary

---
+++ src/components/views/emotionary/Emotionary.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../molecules/navbar/Navbar';
+import Footer from '../../molecules/footer/Footer';
+
+const Emotionary = () => {
+  return (
+    <>
+   <Navbar />
+   <h1 className='text-red-500 aligne-center'>Hola soy el diario de emociones</h1>
+   <Footer />
+   </>
+  );
+}
+
+export default Emotionary;
