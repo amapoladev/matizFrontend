e88f08da8f081a300a6223d6092fd5d52104face
sandovaljohana <sandovaljohana0207@gmail.com>
2024-03-23 01:35:04 +0100

    emotion keep active before select

--- src/components/atoms/buttons/buttonEmotion/ButtonEmotion.jsx
+++ src/components/atoms/buttons/buttonEmotion/ButtonEmotion.jsx
@@ -1,44 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpring, animated } from 'react-spring';
-import useFetch from '../../../../service/UseFetch'; 
 
-const ButtonEmotion = ({ buttonEmotionName, emotionIcon, EmotionButtom, textColorButtom, buttonColor }) => {
-    const [props, set] = useSpring(() => ({ scale: 1, opacity: 1 }));
-    
+const ButtonEmotion = ({ buttonEmotionName, emotionIcon, textColorButtom, buttonColor }) => {
+    const [pressed, setPressed] = useState(false); // Estado para el botón presionado
+
     const handleClick = async () => {
-        console.log("Solicitud de API enviada");
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ emotion: buttonEmotionName }),
-        };
+        console.log("Botón presionado:", buttonEmotionName);
 
-        const { data, loading, error } = useFetch('http://127.0.0.1:8000/api/emotions', requestOptions);
+        // Cambiar el estado de presionado
+        setPressed(!pressed);
 
-        if (loading) {
-            console.log('Cargando...');
-            return;
+        // Aquí puedes enviar la información a tu API
+        try {
+            // Realiza la solicitud a tu API para guardar la información
+            // Por ejemplo:
+            const response = await fetch('http://tu-api.com/users_emotions', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ emotion: buttonEmotionName, pressed: !pressed }), // Pasa el estado de presionado
+            });
+            if (response.ok) {
+                console.log('Emoción guardada con éxito en la base de datos');
+            } else {
+                console.error('Error al guardar la emoción en la base de datos');
+            }
+        } catch (error) {
+            console.error('Error al enviar la solicitud a la API:', error);
         }
-
-        if (error) {
-            console.error('Error:', error);
-            return;
-        }
-
-        console.log('Emoción guardada con éxito:', data);
     };
+
+    const [props, set] = useSpring(() => ({ scale: 1, opacity: 1 }));
 
     return (
         <animated.div
-            className={`card m-2 rounded-full w-24 h-24 glass ${buttonColor} sm:flex`}
-            style={props}
+            className={`card m-2 rounded-full w-24 h-24 glass ${pressed ? 'bg-blue' : buttonColor} sm:flex`}
+            style={{ ...props, cursor: 'pointer' }} // Combina los estilos con el cursor de botón
             onMouseEnter={() => set({ scale: 1.1, opacity: 0.9 })}
             onMouseLeave={() => set({ scale: 1, opacity: 1 })}
-            onClick={handleClick}
+            onClick={handleClick} // Maneja el clic del botón
         >
-            <figure><img className="h-16 pt-2" src={emotionIcon} alt={buttonEmotionName} /></figure>
+            <figure><img className="h-20 pt-4" src={emotionIcon} alt={buttonEmotionName} /></figure>
             <div className="card-body py-0 flex justify-center">
-                <a href=""><h4 className={`card-title text-xs pb-2  ${textColorButtom}`}>{EmotionButtom}</h4></a>
+                <a href=""><h4 className={`card-title text-xs pb-2  ${textColorButtom}`}></h4></a>
             </div>
         </animated.div>
     );
--- src/components/views/emotionary/Emotionary.jsx
+++ src/components/views/emotionary/Emotionary.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import Navbar from '../../molecules/navbar/Navbar';
-import Emotions from '../../molecules/emotions/Emotions';
 import Footer from '../../molecules/footer/Footer';
+import ButtonEmotion from '../../atoms/buttons/buttonEmotion/ButtonEmotion';
+import useFetch from '../../../service/UseFetch';
 
 const Emotionary = () => {
+  const { data } = useFetch('http://127.0.0.1:8000/api/emotions');
+
+  if (!data || !data.emotions) {
+    return <>Loading</>;
+  }
+
+  const emotions = data.emotions;
+  
   return (
     <>
       <Navbar />
-      <h1 className='mt-32 ml-8 text-blue font-bold aligne-center'>¿Cómo te sientes hoy?</h1>
-      <Emotions />
-      <h2 className='mt-2 ml-8 text-blue font-bold aligne-center'>Este mes</h2>
-      
+      <h1 className='mt-32 ml-8 text-blue font-bold align-center'>¿Cómo te sientes hoy?</h1>
+
+      <div className='container mx-auto flex flex-wrap justify-center gap-8 mt-10 p-4'>
+        {emotions.map((emotion) => (
+          <ButtonEmotion
+          key={emotion.id}
+          emotionIcon={emotion.emotion_url}
+          buttonEmotionName={emotion.emotion}
+          buttonColor="bg-blueLight"
+          className="my-2"
+        />
+        ))}
+      </div>
+      <h2 className='mt-2 ml-8 text-blue font-bold align-center'>Este mes</h2>
 
       <Footer />
     </>
